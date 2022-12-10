efa5cb11bb5c88942a9d7d5c75afc2ae87ee752d
Evan Anderson <evndanderson@gmail.com>
2022-12-09 19:28:33 -0600

    Added QOTD command Installed babel syntax import assertions plugin Added stricter equality checking for tests

--- commands.js
+++ commands.js
@@ -1,3 +1,5 @@
+import questions from './questions.json' assert { type: 'json' };
+
 export const commands = {
   PING_COMMAND: {
     name: 'ping',
@@ -8,6 +10,12 @@ export const commands = {
   HELP_COMMAND: {
     name: 'help',
     description: 'Displays a list of commands to the user',
+    type: 1,
+  },
+
+  QOTD_COMMAND: {
+    name: 'qotd',
+    description: 'Asks a question of the day',
     type: 1,
   },
 };
@@ -21,7 +29,6 @@ export const responses = {
   ping: function ping(req) {
     // Send a message into the channel where command was triggered from
     const now = Date.now();
-    // eslint-disable-next-line no-bitwise
     const timestamp = TimestampFromSnowFlake(req.body.id);
 
     return {
@@ -42,6 +49,24 @@ export const responses = {
       type: 4,
       data: {
         content: temp,
+      },
+    };
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  qotd: function qotd(req) {
+    const question = questions[Math.floor(Math.random() * 500)];
+    return {
+      type: 4,
+      data: {
+        embeds: [
+          {
+            title: 'Question of the Day',
+            type: 'rich',
+            description: question,
+            color: 0x5865F2,
+          },
+        ],
       },
     };
   },
--- tests/commands.test.js
+++ tests/commands.test.js
@@ -2,15 +2,14 @@
 import { responses, TimestampFromSnowFlake } from '../commands.js';
 
 test('help', () => {
-  expect(responses.help({})).toEqual({
-    type: 4,
-    data: {
-      content: '/ping - Says Pong\n/help - Displays a list of commands to the user\n',
-    },
-  });
+  expect(responses.help({}).data.content).toBe(
+    `/ping - Says Pong
+/help - Displays a list of commands to the user
+/qotd - Asks a question of the day\n`,
+  );
 });
 
 test('timestamp-from-snowflake', () => {
   expect(TimestampFromSnowFlake('1049899707675189360'))
-    .toEqual(1670385996503);
+    .toBe(1670385996503);
 });
