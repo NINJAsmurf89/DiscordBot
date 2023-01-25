3a5a7df4d427e434b44770ebf60a9f80fd5be854
Evan Anderson <evndanderson@gmail.com>
2023-01-24 20:06:02 -0600

    Changed button color to green

--- src/interactions/application-commands.js
+++ src/interactions/application-commands.js
@@ -99,7 +99,7 @@ export function button(req) {
             {
               type: ComponentType.BUTTON,
               label: 'Click me!',
-              style: ButtonStyle.PRIMARY,
+              style: ButtonStyle.SUCCESS,
               custom_id: 'button',
             },
           ],
